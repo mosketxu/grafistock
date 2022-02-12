bbf3dda2e13820e91d20fd099b1ce0aeadc8d9cf
Alexander Arregui <mosketxu@gmail.com>
2022-02-12 12:39:39 +0100

    filtros stock

--- app/Http/Livewire/StockMovimientos.php
+++ app/Http/Livewire/StockMovimientos.php
@@ -3,22 +3,20 @@
 namespace App\Http\Livewire;
 
 use Livewire\WithPagination;
-use App\Http\Livewire\DataTable\WithBulkActions;
 use Livewire\Component;
-use App\Models\{StockMovimiento,Entidad,ProductoMaterial,Producto, ProductoAcabado, ProductoFamilia, Solicitante};
+use App\Models\{StockMovimiento,Entidad,Producto, Solicitante};
 
 
 class StockMovimientos extends Component
 {
-    use WithPagination, WithBulkActions;
+    use WithPagination;
 
     public $search='';
     public $filtroclipro='';
     public $filtromaterial='';
     public $filtrofamilia='';
     public $filtroacabado='';
-    public $filtroproducto='';
-    public $filtrodescripcion='';
+    public $filtrotipo='';
     public $filtrosolicitante='';
     public $filtroanyo='';
     public $filtromes='';
@@ -26,44 +24,130 @@ class StockMovimientos extends Component
 
     public function render()
     {
-        if($this->selectAll) $this->selectPageRows();
-        $stocks = $this->rows;
 
-        $proveedores=Entidad::query()
-            ->whereHas('pedidos')
-            ->orderBy('entidad')
-            ->get();
-        $familias=ProductoFamilia::orderBy('nombre')->get();
-        $materiales=ProductoMaterial::orderBy('nombre')->get();
         $solicitantes=Solicitante::orderBy('nombre')->get();
-        $acabados=ProductoAcabado::orderBy('nombre')->get();
+        $proveedores=Entidad::whereHas('pedidos')->orderBy('entidad')->get();
+        $materiales= Producto::query()
+            ->join('producto_materiales','producto_materiales.id','=','productos.material_id')
+            ->select('producto_materiales.id', 'producto_materiales.nombre')
+            ->groupBy('material_id')
+            ->search('referencia',$this->search)
+            ->orSearch('descripcion',$this->search)
+            ->when($this->filtroclipro!='', function ($query){$query->where('entidad_id',$this->filtroclipro);})
+            ->when($this->filtrofamilia!='', function ($query){$query->where('familia_id',$this->filtrofamilia);})
+            ->when($this->filtroacabado!='', function ($query){$query->where('acabado_id',$this->filtroacabado);})
+            ->when($this->filtrotipo!='', function ($query){$query->where('tipo_id',$this->filtrotipo);})
+            ->orderBy('producto_materiales.nombre')
+            ->get();
 
-        $productos=Producto::orderBy('referencia')
+        $familias= Producto::query()
+            ->join('producto_familias','producto_familias.id','=','productos.familia_id')
+            ->select('producto_familias.id', 'producto_familias.nombre')
+            ->groupBy('familia_id')
+            ->search('referencia',$this->search)
+            ->orSearch('descripcion',$this->search)
+            ->when($this->filtroclipro!='', function ($query){$query->where('entidad_id',$this->filtroclipro);})
+            ->when($this->filtromaterial!='', function ($query){$query->where('material_id',$this->filtromaterial);})
+            ->when($this->filtroacabado!='', function ($query){$query->where('acabado_id',$this->filtroacabado);})
+            ->when($this->filtrotipo!='', function ($query){$query->where('tipo_id',$this->filtrotipo);})
+            ->orderBy('producto_familias.nombre')
+            ->get();
+
+        $acabados= Producto::query()
+            ->join('producto_acabados','producto_acabados.id','=','productos.acabado_id')
+            ->select('producto_acabados.id', 'producto_acabados.nombre')
+            ->groupBy('acabado_id')
+            ->search('referencia',$this->search)
+            ->orSearch('descripcion',$this->search)
+            ->when($this->filtroclipro!='', function ($query){$query->where('entidad_id',$this->filtroclipro);})
+            ->when($this->filtromaterial!='', function ($query){$query->where('material_id',$this->filtromaterial);})
+            ->when($this->filtrofamilia!='', function ($query){$query->where('familia_id',$this->filtrofamilia);})
+            ->when($this->filtrotipo!='', function ($query){$query->where('tipo_id',$this->filtrotipo);})
+            ->orderBy('producto_acabados.nombre')
+            ->get();
+
+        $tipos= Producto::query()
+            ->join('producto_tipos','producto_tipos.id','=','productos.tipo_id')
+            ->select('producto_tipos.id', 'producto_tipos.nombre')
+            ->groupBy('tipo_id')
+            ->search('referencia',$this->search)
+            ->orSearch('descripcion',$this->search)
+            ->when($this->filtroclipro!='', function ($query){$query->where('entidad_id',$this->filtroclipro);})
+            ->when($this->filtromaterial!='', function ($query){$query->where('material_id',$this->filtromaterial);})
+            ->when($this->filtroacabado!='', function ($query){$query->where('acabado_id',$this->filtroacabado);})
+            ->when($this->filtrofamilia!='', function ($query){$query->where('familia_id',$this->filtrofamilia);})
+            ->orderBy('producto_tipos.nombre')
+            ->get();
+
+        $productos=Producto::query()
+            ->with('entidad','material','acabado','tipo')
+            ->search('referencia',$this->search)
+            ->orSearch('descripcion',$this->search)
+            ->when($this->filtrofamilia!='', function ($query){
+                $query->where('familia_id',$this->filtrofamilia);
+                })
+            ->when($this->filtromaterial!='', function ($query){
+                $query->where('material_id',$this->filtromaterial);
+                })
             ->when($this->filtroclipro!='', function ($query){
                 $query->where('entidad_id',$this->filtroclipro);
+                })
+            ->when($this->filtroacabado!='', function ($query){
+                $query->where('acabado_id',$this->filtroacabado);
+                })
+            ->when($this->filtrotipo!='', function ($query){
+                $query->where('tipo_id',$this->filtrotipo);
+                })
+            ->orderBy('referencia','asc')
+            ->get();
+
+        $stocks =  StockMovimiento::query()
+            ->join('productos','productos.id','stock_movimientos.producto_id')
+            ->select('stock_movimientos.*','productos.material_id')
+            ->with('producto.entidad:id,entidad')
+            ->with('solicitante')
+            ->searchYear('fechamovimiento',$this->filtroanyo)
+            ->searchMes('fechamovimiento',$this->filtromes)
+            ->when($this->filtrosolicitante!='', function ($query){
+                $query->where('solicitante_id',$this->filtrosolicitante);
             })
             ->when($this->filtromaterial!='', function ($query){
                 $query->where('material_id',$this->filtromaterial);
             })
-            ->when($this->filtrofamilia!='', function ($query){
-                $query->where('familia_id',$this->filtrofamilia);
-            })
             ->when($this->filtroacabado!='', function ($query){
                 $query->where('acabado_id',$this->filtroacabado);
             })
-            ->search('descripcion',$this->filtrodescripcion)
-            ->get();
+            ->when($this->filtroclipro!='', function ($query){
+                $query->whereHas('producto',function($q){
+                    $q->where('entidad_id',$this->filtroclipro);
+                });
+            })
+            ->searchYear('fechamovimiento',$this->filtroanyo)
+            ->searchMes('fechamovimiento',$this->filtromes)
+            ->search('referencia',$this->search)
+            ->orSearch('descripcion',$this->search)
+            ->orderBy('stock_movimientos.fechamovimiento','desc')
+            ->paginate(15);
 
-        return view('livewire.stock-movimientos',compact('stocks','proveedores','productos','familias','acabados','materiales','solicitantes'));
+        return view('livewire.stock-movimientos',compact('stocks','proveedores','productos','familias','acabados','tipos','materiales','solicitantes'));
     }
 
+    public function updatingSearch(){
+        $this->resetPage();
+    }
     public function updatingFiltroclipro(){
         $this->resetPage();
     }
-    public function updatingFiltroproducto(){
+    public function updatingFiltromaterial(){
         $this->resetPage();
     }
-    public function updatingFiltromaterial(){
+    public function updatingFiltrofamilia(){
+        $this->resetPage();
+    }
+    public function updatingFiltroacabado(){
+        $this->resetPage();
+    }
+    public function updatingFiltrotipo(){
         $this->resetPage();
     }
     public function updatingFiltrosolicitante(){
@@ -75,67 +159,9 @@ class StockMovimientos extends Component
     public function updatingFiltromes(){
         $this->resetPage();
     }
-    public function updatingDescripcion(){
-        $this->resetPage();
-    }
-
-    public function getRowsQueryProperty(){
-        return StockMovimiento::query()
-        ->join('productos','productos.id','stock_movimientos.producto_id')
-        ->select('stock_movimientos.*','productos.material_id')
-        ->with('producto')
-        ->with('producto.entidad')
-        ->with('solicitante')
-        ->searchYear('fechamovimiento',$this->filtroanyo)
-        ->searchMes('fechamovimiento',$this->filtromes)
-        ->when($this->filtroproducto!='', function ($query){
-            $query->where('producto_id',$this->filtroproducto);
-        })
-        ->when($this->filtrosolicitante!='', function ($query){
-            $query->where('solicitante_id',$this->filtrosolicitante);
-        })
-        ->when($this->filtrodescripcion!='', function ($query){
-            $query->where('descripcion','LIKE','%'.$this->filtrodescripcion.'%');
-        })
-        ->when($this->filtromaterial!='', function ($query){
-            $query->where('material_id',$this->filtromaterial);
-        })
-        ->when($this->filtroclipro!='', function ($query){
-            $query->whereHas('producto',function($q){
-                $q->where('entidad_id',$this->filtroclipro);
-            });
-        })
-        ->searchYear('fechamovimiento',$this->filtroanyo)
-        ->searchMes('fechamovimiento',$this->filtromes)
-        ->orderBy('stock_movimientos.fechamovimiento','desc');
-        // ->paginate(5); solo contemplo la query, no el resultado. Luego pongo el resultado: get, paginate o lo que quiera
-
-    }
-
-    public function getRowsProperty(){
-        return $this->rowsQuery->paginate();
-    }
-
-    public function exportSelected(){
-        //toCsv es una macro a n AppServiceProvider
-        return response()->streamDownload(function(){
-            echo $this->selectedRowsQuery->toCsv();
-        },'movimientos.csv');
-
-        $this->dispatchBrowserEvent('notify', 'CSV Movimientos descargados!');
-    }
-
-    public function deleteSelected(){
-        $deleteCount = $this->selectedRowsQuery->count();
-        $this->selectedRowsQuery->delete();
-        $this->showDeleteModal = false;
-
-        $this->dispatchBrowserEvent('notify', $deleteCount . ' Movimientos eliminados!');
-    }
 
     public function delete($stockId)
     {
-
         $stock = StockMovimiento::find($stockId);
         if ($stock) {
             $stock->delete();
