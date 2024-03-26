362e5f767da2c99d01704f35abefd8eadf2b13d4
Alexander Arregui <mosketxu@gmail.com>
2024-03-26 17:10:07 +0100

    bug

--- app/Http/Livewire/PresupLineaDetalles.php
+++ app/Http/Livewire/PresupLineaDetalles.php
@@ -44,7 +44,8 @@ class PresupLineaDetalles extends Component
         else
             $preciominimo=Producto::find($presupaccion->accionproducto_id)->preciocoste;
 
-        if(!Auth::user()->hasRole('Admin') && $presupaccion->producto->referencia=="Pedido Mínimo")
+        // if(!Auth::user()->hasRole('Admin') && $presupaccion->producto->referencia=="Pedido Mínimo")
+        if(!Auth::user()->hasRole('Admin'))
             $this->dispatchBrowserEvent("notify", "Este valor solo lo puede modificar Dirección Comercial.");
         else{
             //Preparamos y validamos antes de actualizar
